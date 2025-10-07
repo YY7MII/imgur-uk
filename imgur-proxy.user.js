73b1a8390a2711f25ccdae60cff870988e4a64df
YY7MII <yy7mii@gmail.com>
2025-10-07 13:00:34 +0100

    Create imgur-proxy.user.js

---
+++ imgur-proxy.user.js
@@ -0,0 +1,18 @@
+// ==UserScript==
+// @name         Yummy Imgur Proxy
+// @namespace    yy7mii's imgur proxy for userscripts :p
+// @version      1.0
+// @description  Replace all i.imgur.com text with imgur-uk.vercel.app
+// @match        *://*/*
+// @run-at       document-end
+// @grant        none
+// @updateURL    https://raw.githubusercontent.com/YY7MII/imgur-uk/main/imgur-proxy.user.js
+// @downloadURL  https://raw.githubusercontent.com/YY7MII/imgur-uk/main/imgur-proxy.user.js
+// @noframes
+// ==/UserScript==
+
+(function () {
+  'use strict';
+  // Replace all instances of i.imgur.com -> imgur-uk.vercel.app
+  document.body.innerHTML = document.body.innerHTML.replaceAll('i.imgur.com', 'imgur-uk.vercel.app');
+})();
